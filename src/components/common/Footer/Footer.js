320a40f157be1774c4ec12c783d72a90a8583921
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-06-09 00:57:37 +0200

    Add NavBar and Footer with styling

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -2,8 +2,11 @@
 
 const Footer = () => {
   return(
-    <div>Footer</div>
-  )
+    <footer className='text-center text-muted py-4'>
+      <p>Copyright © BlogApp 2023</p>
+      <p>Created by Patryk Szwiec</p>
+    </footer>
+    )
 }
 
 export default Footer;
--- src/components/common/NavBar/NavBar.js
+++ src/components/common/NavBar/NavBar.js
@@ -1,9 +1,18 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-  return(
-    <div>NavBar</div>
-  )
-
+    return (
+        <Navbar bg='primary' variant='dark' expand='md' className='mt-4 mb-4 rounded'>
+            <Container>
+                <Navbar.Brand>Blog.app</Navbar.Brand>
+                <Nav>
+                    <Nav.Link as={NavLink} to='/'>Home</Nav.Link>
+                    <Nav.Link as={NavLink} to='/about'>About</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
 }
 
 export default NavBar;
