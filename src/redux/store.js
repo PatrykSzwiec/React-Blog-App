031964dff30911440da3119fd579f42ad5eec31a
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-06-08 22:45:55 +0200

    Add redux, react router, reactbootstrap and dart-ssas

---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+import { legacy_createStore as createStore } from 'redux';
+import initialState from './initialState';
+import { combineReducers } from 'redux';
+
+const subreducers = {
+}
+
+const reducer = combineReducers(subreducers);
+
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
